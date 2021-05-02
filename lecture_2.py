d5aa6312b4c49f5384951b41b3bcd8ae1cd3abfe
muhammad-badran <43704228+muhammad-badran@users.noreply.github.com>
2021-05-02 17:04:35 +0200

    Update lecture_2.py

--- lecture_2.py
+++ lecture_2.py
@@ -16,12 +16,24 @@ for i in range(1000):
   #print(x,y)
   points.append((x, y))
   
+# Gradient Descent
 def F(w):
   return sum((w.dot(x) - y)**2 for x, y in points) / len(points)
 
 def dF(w):
   return sum(2*(w.dot(x) - y) * x for x, y in points) / len(points)
-  
+
+# Stocastic Gradient Descent
+def sF(w, i):
+  x , y = points[i]
+  return sum((w.dot(x) - y)**2
+
+def sdF(w, i):
+  x , y = points[i]
+  return 2*(w.dot(x) - y) * x
+
+
+
 #####################################
 
 # Algorithms: how we compute it
@@ -30,10 +42,25 @@ def gradientDescent(F, dF, d):
   # Gradient descent
   w = np.zeros(d)
   eta = 0.01
-  for t in range(100):
+  for t in range(1000):
     value = F(w)
     gradient = dF(w)
     w = w - eta * gradient
     print('iteration {}: w = {}, F(w) = {}'.format(t, w, value))
 
+def stocasticGradientDescent(sF, sdF, d, n):
+  # Stocastic Gradient descent
+  w = np.zeros(d)
+  eta = 1
+  numUpdates = 0
+  for t in range(1000):
+    for i in range(n):
+      value = sF(w, i)
+      gradient = sdF(w, i)   
+      numUpdates += 1
+      eta = 1.0 / numUpdates
+      w = w - eta * gradient
+    print('iteration {}: w = {}, F(w) = {}'.format(t, w, value))
+             
+             
 gradientDescent(F, df, d)
