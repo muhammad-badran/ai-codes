8b824df09e19d4b94f80ebafc3e7a9dc8794a6f4
muhammad-badran <43704228+muhammad-badran@users.noreply.github.com>
2021-05-07 03:28:17 +0200

    Update lecture_5.py

--- lecture_5.py
+++ lecture_5.py
@@ -16,6 +16,41 @@ Class TransportationProblem(object):
     if state * 2 < self.N:
       result.append(('tram', state * 2, 2))
     return result
+
+## Algorithms
+def printSolution(solution):
+  totalCost, history = solution
+  print('total cost: {}'.format(totalCost))
+  for item in hisotry:
+    print(item)
+
+def backtrackingSearch(problem):
+  # Best solution found so far (dictionary because of python scoping technicaility)
+  best = {
+    'cost': float(+inf),
+    'history': None
+  }
+  def recurse(state, history, totalCost):
+    # At state, having undergone history, accumulated total cost.
+    # Explore the rest of the subtree under state.
+    
+    if problem.isEnd(state):
+      # Update the best solution so far
+      if totalCost < best['cost']:
+        best['cost'] = totalCost
+        best['history'] = history
+     return
+  # Recurse of children
+  for action, newState, cost in problem.succAndCost(state):
+    recurse(newState, history+[(action, newState, cost)], totalCost + cost
+            
+  recurse(problem.startState(), history[], totalCost = 0)
+  return (best['cost'], best['history'])
+  
+  
+  
+  
+  
   
 problem = TransportationProblem(N=10)
 print(problem.succAndCost(3))
